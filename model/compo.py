cfcaceb55a46d3b3e91444433f3844fabba4bdf4
84368 <843683531@qq.com>
2022-12-01 14:36:29 +0800

    补充注释

--- model/compo.py
+++ model/compo.py
@@ -1,6 +1,6 @@
 class Compo:#所有id=0的为state，即background/node
     def __init__(self, x, y, height, width):#(组件id，组件相对坐标)
-        #存储左下角的坐标(默认左下角为0,0)
+        #存储左上角的坐标(默认左上角为0,0)
         self.x = x
         self.y = y
         #存储该组件的高和宽
